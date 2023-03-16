058643ab87d4a29c826197cae9ec70cd8db6f0c0
Rafael Medeiros <rafaelfufcg@gmail.com>
2023-03-16 10:56:23 -0300

    Ajustando layout main activity e criando classe Pessoa

---
+++ AppListaCurso/app/src/main/java/devandroid/rafael/applistacurso/model/Pessoa.java
@@ -0,0 +1,45 @@
+package devandroid.rafael.applistacurso.model;
+
+public class Pessoa {
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefone;
+
+    public Pessoa() {
+
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
--- AppListaCurso/app/src/main/java/devandroid/rafael/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/rafael/applistacurso/view/MainActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import devandroid.rafael.applistacurso.R;
+import devandroid.rafael.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+
+        pessoa.setPrimeiroNome("Rafael");
+        pessoa.setSobreNome("Medeiros");
+        pessoa.setCursoDesejado("ADM");
+        pessoa.setTelefone("83 9999 - 9999");
+
+        int parada = 0;
+
     }
 }
